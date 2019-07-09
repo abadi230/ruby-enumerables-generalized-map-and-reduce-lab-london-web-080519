1fbda452703fb35aa5b97e306e4f9451578ee6d3
Abdullah Bajaman <ambajaman@gmail.com>
2019-07-09 15:41:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,5 +4,5 @@ def map(array)
 end
 
 def reduce(array, start_point = 0)
-  array.reduce(start_point){|element, n| element, yield(n)}
+  array.reduce(start_point){|element, n|  yield(element,n)}
 end
