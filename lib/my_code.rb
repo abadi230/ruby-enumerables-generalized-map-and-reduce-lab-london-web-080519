ca0792abbe430fccb1b1e044cd9e825907316f61
Abdullah Bajaman <ambajaman@gmail.com>
2019-07-09 16:04:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,5 +5,5 @@ end
 
 def reduce(array, start_point = 0)
   array.reduce(start_point){|element, n|  
-    n == true yield(!!element, n) : yield(element,n)}
+    n == true ? yield(!!element, n) : yield(element,n) }
 end
